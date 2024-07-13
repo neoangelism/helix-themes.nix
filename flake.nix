71c0d016003495b268d4a9b4382355fda26640c3
eureka-cpu <github.eureka@gmail.com>
2024-07-12 22:00:39 -0700

    merge themes into flake attribute set to avoid system field

--- flake.nix
+++ flake.nix
@@ -1,39 +1,37 @@
 {
   description = "Declarative Helix editor themes for home-manager";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nil
+              nixpkgs-fmt
+            ];
+          };
 
-        kanabox_default = import ./themes/kanabox_default.nix;
-        gruvbox_material_dark_medium = import ./themes/gruvbox_material_dark_medium.nix;
-        urban_owl = import ./themes/urban_owl.nix;
-      in
-      rec {
+          formatter = pkgs.nixpkgs-fmt;
+        }) // rec
+    {
+      gruvbox_material_dark_medium = import ./themes/gruvbox_material_dark_medium.nix;
+      kanabox_default = import ./themes/kanabox_default.nix;
+      urban_owl = import ./themes/urban_owl.nix;
+
+      themes = {
         inherit
-          kanabox_default
           gruvbox_material_dark_medium
+          kanabox_default
           urban_owl;
-
-        themes = {
-          inherit
-            kanabox_default
-            gruvbox_material_dark_medium
-            urban_owl;
-        };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nil
-            nixpkgs-fmt
-          ];
-        };
-
-        formatter = pkgs.nixpkgs-fmt;
-      });
+      };
+    };
 }
